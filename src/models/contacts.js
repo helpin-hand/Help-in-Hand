ae126d617b11470467fcaac7636e6e513e4db16c
Iamayush2901 <hande.ayush29@gmail.com>
2023-06-10 21:57:19 +0530

    nodejs project first commit

---
+++ src/models/contacts.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const employeSchema = new mongoose.Schema({
+    name : {
+        type:String,
+        required:true,
+    },
+    email : {
+        type:String,
+        required:true,
+    },
+    message : {
+        type:String,
+        required:true
+    },
+})
+
+const Contact = new mongoose.model("Contact", employeSchema);
+
+module.exports = Contact;
